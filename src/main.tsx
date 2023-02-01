2ba45fd8f275a3a2f1738ba7e5ab1068ef9bdc14
keyhr <keyhre@gmail.com>
2023-02-02 01:00:37 +0900

    Revert "[update]"
    This reverts commit f79f0a02924df03bf5befcc40fdd24f60c695921.

--- src/main.tsx
+++ src/main.tsx
@@ -4,21 +4,17 @@ import { App } from "./App";
 import "./style.scss";
 import "./query.scss";
 
-// /** @ts-ignore */
-// request.html((_, div) => {
-//   div.innerHTML = `
-//   <link rel="preconnect" href="https://fonts.googleapis.com">
-//   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-//   <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@800&display=swap" rel="stylesheet">
-//     <div id="root"></div>
-//   `;
-//   const i = document.getElementsByName("script") as NodeListOf<HTMLInputElement>;
-//   i[0].disabled = true;
-//   const container = document.getElementById("root") as HTMLElement;
-//   const root = createRoot(container);
-//   root.render(<App />);
-// }).run();
-
-const container = document.getElementById("tetris-root") as HTMLElement;
-const root = createRoot(container);
-root.render(<App />);
+/** @ts-ignore */
+request.html((_, div) => {
+  div.innerHTML = `
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Saira+Condensed:wght@800&display=swap" rel="stylesheet">
+    <div id="root"></div>
+  `;
+  const i = document.getElementsByName("script") as NodeListOf<HTMLInputElement>;
+  i[0].disabled = true;
+  const container = document.getElementById("root") as HTMLElement;
+  const root = createRoot(container);
+  root.render(<App />);
+}).run();
